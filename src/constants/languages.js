a53270918b58dc68d68e50b80f669da50d4a7179
Akhil092004 <akhiljain092004@gmail.com>
2025-02-02 02:54:20 +0530

    Add languages constant for multilingual support

---
+++ src/constants/languages.js
@@ -0,0 +1,24 @@
+const languages = [
+    {
+        key: 'en',
+        value: 'English'
+    },
+    {
+        key: 'fr',
+        value: 'French'
+    }, 
+    {
+        key: 'es',
+        value: 'Spanish'
+    },
+    {
+        key:'hi',
+        value:'Hindi'
+    },
+    {
+        key:'bn',
+        value:'Bengali'
+    }
+]
+
+export default languages
